a4e03f555925eb3088cc384d3f097aa61ab24db5
olga.potapova <ol.potapova20_01@mail.ru>
2020-12-07 00:51:03 +0300

    Add authorization page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Chat from './components/Chat';
+import Authorization from './components/Authorization';
 
 function App(props) {
   return (
     <div className="App">
-      <Chat />
+      <Authorization />
     </div>
   );
 }
---
+++ src/components/Authorization.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        margin: '15%',
+        padding: theme.spacing(3, 2),
+        borderTop: '1px solid #C0C0C0',
+        minWidth: '300px'
+    },
+    title: {
+        width: 'inherit',
+        height: '50px',
+        borderBottom: '1px solid #C0C0C0'
+    },
+    textBox: {
+        width: '90%',
+        height: '50px'
+    },
+    button: {
+        width: '100px'
+    },
+    field: {
+        margin: '15px'
+    }
+  }));
+
+export default function Authorization() {
+    const classes = useStyles();
+
+    const [emailValue, changeEmailValue] = useState('');
+
+    const [passwordValue, changePasswordValue] = useState('');
+
+    return (
+        <div>
+            <Paper className = {classes.root}>
+                <div className = {classes.title}>
+                    <Typography variant="h5" component="h5">
+                        Authorization
+                    </Typography>
+                </div>
+                <div className = {classes.field}>
+                    <TextField 
+                        id="outlined-basic" 
+                        label="E-mail" 
+                        variant="outlined" 
+                        className = {classes.textBox}
+                        value = {emailValue}
+                        onChange = {e => changeEmailValue(e.target.value)}
+                        type="email"
+                    />
+                </div>
+                <div className = {classes.field}>
+                    <TextField 
+                        id="outlined-basic" 
+                        label="Password" 
+                        variant="outlined" 
+                        className = {classes.textBox}
+                        value = {passwordValue}
+                        onChange = {e => changePasswordValue(e.target.value)}
+                        type="password"
+                    />
+                </div>
+                <div className = {classes.field}>
+                    <Button 
+                        variant="contained" 
+                        color="primary" 
+                        className = {classes.button}
+                        onClick={() => {
+                            fetch('', {})}}
+                    >
+                        Sign in
+                    </Button>
+                </div>
+            </Paper>
+        </div>
+    )
+}
